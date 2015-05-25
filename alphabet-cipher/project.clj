e7a8ebef281af309954d738734d3e01b9b5f15b4
Richard W. Norton <rwtnorton@gmail.com>
2015-05-24 22:46:45 -0700

    Solved.

--- alphabet-cipher/project.clj
+++ alphabet-cipher/project.clj
@@ -1,6 +1,6 @@
 (defproject alphabet-cipher "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "My solution to the alphabet cipher kata from Living Clojure."
+  :url "https://github.com/rwtnorton/wonderland-clojure-katas/tree/master/alphabet-cipher"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,41 @@
 (ns alphabet-cipher.coder)
 
-(defn encode [keyword message]
-  "encodeme")
+(defn lazy-seq->str [s]
+  (apply str (vec s)))
 
-(defn decode [keyword message]
-  "decodeme")
+(def alphabet (->> (range (int \a) (inc (int \z)))
+                   (map char)
+                   lazy-seq->str))
+
+(defn distance-from-a [c]
+  (- (int c) (int \a)))
+
+(defn stretch-keyword [keyword n]
+  (->> (seq keyword)
+       cycle
+       (take n)
+       lazy-seq->str))
+
+(defn encode-char [kw-char msg-char]
+  (let [n (+ (distance-from-a kw-char)
+             (distance-from-a msg-char))
+        i (mod n (count alphabet))]
+    (get alphabet i)))
+
+(defn decode-char [kw-char enc-char]
+  (let [n (- (distance-from-a enc-char)
+             (distance-from-a kw-char))
+        i (mod n (count alphabet))]
+    (get alphabet i)))
+
+(defn gen-coding-fn [coding-char-fn]
+  (fn [keyword message]
+    (let [stretched (stretch-keyword keyword (count message))]
+      (->> (map vector stretched message)
+           (map (fn [[k m]] (coding-char-fn k m)))
+           lazy-seq->str))))
+
+(def encode (gen-coding-fn encode-char))
+
+(def decode (gen-coding-fn decode-char))
+
