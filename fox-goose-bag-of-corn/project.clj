1c9cebf4777b2d09fc840612613d38c1ce40c385
Richard W. Norton <rwtnorton@gmail.com>
2015-05-25 16:01:38 -0700

    Add disallowed tests and goal test.

--- fox-goose-bag-of-corn/project.clj
+++ fox-goose-bag-of-corn/project.clj
@@ -1,6 +1,6 @@
 (defproject fox-goose-bag-of-corn "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "My solution to the Fox, Goose and Bag of Corn Kata from Learning Clojure."
+  :url "https://github.com/rwtnorton/wonderland-clojure-katas/tree/master/fox-goose-bag-of-corn"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -1,6 +1,35 @@
 (ns fox-goose-bag-of-corn.puzzle)
 
-(def start-pos [[[:fox :goose :corn :you] [:boat] []]])
+(def start-pos [
+                [[:fox :goose :corn :you]
+                 [:boat]
+                 []]])
+
+(defn goal? [[left-bank river right-bank]]
+  (and (empty? left-bank)
+       (= #{:boat} (set river))
+       (= #{:fox :goose :corn :you} (set right-bank))))
+
+(defn unattended-on-bank? [malcontents bank]
+  (let [b (set bank)]
+    (and (not (:you b))
+         (every? b malcontents))))
+
+(def fox-alone-with-goose-on-bank?
+  (partial unattended-on-bank? [:fox :goose]))
+
+(def goose-alone-with-corn-on-bank?
+  (partial unattended-on-bank? [:goose :corn]))
+
+(defn banks? [predicate [left-bank _ right-bank]]
+  (or (predicate left-bank)
+      (predicate right-bank)))
+
+(defn fox-alone-with-goose? [state]
+  (banks? fox-alone-with-goose-on-bank? state))
+
+(defn goose-alone-with-corn? [state]
+  (banks? goose-alone-with-corn? state))
 
 (defn river-crossing-plan []
   start-pos)
