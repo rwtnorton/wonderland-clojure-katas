a5add1ae18ba13a620337a2b15aa502fb9c6f377
Richard W. Norton <rwtnorton@gmail.com>
2015-05-25 18:23:48 -0700

    Add actions.

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -1,7 +1,8 @@
 (ns fox-goose-bag-of-corn.puzzle)
 
 (def start-pos [
-                [[:fox :goose :corn :you]
+                [
+                 [:fox :goose :corn :you]
                  [:boat]
                  []]])
 
@@ -10,26 +11,85 @@
        (= #{:boat} (set river))
        (= #{:fox :goose :corn :you} (set right-bank))))
 
-(defn unattended-on-bank? [malcontents bank]
-  (let [b (set bank)]
+(defn unattended-at? [malcontents place]
+  (let [b (set place)]
     (and (not (:you b))
          (every? b malcontents))))
 
-(def fox-alone-with-goose-on-bank?
-  (partial unattended-on-bank? [:fox :goose]))
+(def fox-alone-with-goose-at?
+  (partial unattended-at? [:fox :goose]))
 
-(def goose-alone-with-corn-on-bank?
-  (partial unattended-on-bank? [:goose :corn]))
-
-(defn banks? [predicate [left-bank _ right-bank]]
-  (or (predicate left-bank)
-      (predicate right-bank)))
+(def goose-alone-with-corn-at?
+  (partial unattended-at? [:goose :corn]))
 
 (defn fox-alone-with-goose? [state]
-  (banks? fox-alone-with-goose-on-bank? state))
+  (some fox-alone-with-goose-at? state))
 
 (defn goose-alone-with-corn? [state]
-  (banks? goose-alone-with-corn? state))
+  (some goose-alone-with-corn-at? state))
+
+(defn on-left-bank? [[left-bank _ _]]
+  (:you (set left-bank)))
+
+(defn on-boat? [[_ river _]]
+  (:you (set river)))
+
+(defn on-right-bank? [[_ _ right-bank]]
+  (:you (set right-bank)))
+
+(defn remove-at [goners place]
+  (vec (remove (set goners) place)))
+
+(defn move->left-bank->boat [[left-bank river right-bank]]
+  [(remove-at [:you] left-bank)
+   (conj river :you)
+   right-bank])
+
+(defn move->boat->right-bank [[left-bank river right-bank]]
+  [left-bank
+   (remove-at [:you] river)
+   (conj right-bank :you)])
+
+(defn move->boat->left-bank [[left-bank river right-bank]]
+  [(conj left-bank :you)
+   (remove-at [:you] river)
+   right-bank])
+
+(defn move->right-bank->boat [[left-bank river right-bank]]
+  [left-bank
+   (conj river :you)
+   (remove-at [:you] right-bank)])
+
+(defn haul->left-bank->boat [cargo [left-bank river right-bank]]
+  [(remove-at [:you cargo] left-bank)
+   (conj river :you cargo)
+   right-bank])
+
+(defn haul->boat->right-bank [cargo [left-bank river right-bank]]
+  [left-bank
+   (remove-at [:you cargo] river)
+   (conj right-bank :you cargo)])
+
+(defn haul->right-bank->boat [cargo [left-bank river right-bank]]
+  [left-bank
+   (conj river :you cargo)
+   (remove-at [:you cargo] right-bank)])
+
+(defn haul->boat->left-bank [cargo [left-bank river right-bank]]
+  [(conj left-bank :you cargo)
+   (remove-at [:you cargo] river)
+   right-bank])
+
+(defn haulables-at [place]
+  (vec (remove #{:you :boat} place)))
+
+(defn haulables? [place]
+  (not-empty (haulables-at place)))
+
+(defn next-state [[left-bank river right-bank :as state]]
+  ;; Iteratively explore all valid actions
+  ;; with all valid arguments until goal is reached.
+  state)
 
 (defn river-crossing-plan []
   start-pos)
