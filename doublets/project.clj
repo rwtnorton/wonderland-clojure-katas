c6008d1d73f8264b433e6e89e52275586eb20cfe
Richard W. Norton <rwtnorton@gmail.com>
2015-05-31 10:52:00 -0700

    Initial approach using Levenshtein distance.

--- doublets/project.clj
+++ doublets/project.clj
@@ -1,6 +1,7 @@
 (defproject doublets "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "My solution to the Doublets Kata from Learning Clojure."
+  :url "https://github.com/rwtnorton/wonderland-clojure-katas/tree/master/doublets"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-levy "0.1.0-SNAPSHOT"]])
--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -1,11 +1,23 @@
 (ns doublets.solver
   (:require [clojure.java.io :as io]
-            [clojure.edn :as edn]))
+            [clojure.edn :as edn]
+            [clj-levy.core :as lev]))
 
 (def words (-> "words.edn"
                (io/resource)
                (slurp)
                (read-string)))
 
+(defn words-of-length [n]
+  (filter (fn [w] (= (count w) n)) words))
+
 (defn doublets [word1 word2]
-  "make me work")
+  (cond
+   (not= (count word1) (count word2)) (throw
+                                       (IllegalArgumentException.
+                                        (str "Must have same lengths: "
+                                             word1 word2)))
+   (= word1 word2) []
+   :else (let [words (words-of-length (count word1))
+               ]
+           [word2])))
