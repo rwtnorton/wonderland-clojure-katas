43d44df8a252d0ff638b263554137a406bd8f18b
Richard W. Norton <rwtnorton@gmail.com>
2015-06-06 11:17:26 -0700

    Update project info.

--- magic-square/project.clj
+++ magic-square/project.clj
@@ -1,6 +1,6 @@
 (defproject magic-square "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "My solution to the Magic Square Kata from Learning Clojure."
+  :url "https://github.com/rwtnorton/wonderland-clojure-katas/tree/master/magic-square"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
