497ca371249c1e5fc8eece776b7f92b554f6dd4a
Richard W. Norton <rwtnorton@gmail.com>
2015-06-06 12:37:01 -0700

    Solved.

--- magic-square/project.clj
+++ magic-square/project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/rwtnorton/wonderland-clojure-katas/tree/master/magic-square"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/math.combinatorics "0.1.1"]])
--- magic-square/src/magic_square/puzzle.clj
+++ magic-square/src/magic_square/puzzle.clj
@@ -1,8 +1,40 @@
-(ns magic-square.puzzle)
+(ns magic-square.puzzle
+  (:require [clojure.math.combinatorics :as combo]))
 
 (def values [1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0])
 
+(defn sum-by-rows [vs]
+  (->> vs
+       (partition 3)
+       (map #(reduce + %))))
+
+(defn nth-all [vs indices]
+  (map (fn [i] (nth vs i)) indices))
+
+(defn sum-by-cols [vs]
+  [(reduce + (nth-all vs [0 3 6]))
+   (reduce + (nth-all vs [1 4 7]))
+   (reduce + (nth-all vs [2 5 8]))])
+
+(defn sum-by-diagonals [vs]
+  [(reduce + (nth-all vs [0 4 8]))
+   (reduce + (nth-all vs [2 4 6]))])
+
+(defn magic? [vs]
+  (and
+   (apply = (sum-by-rows vs))
+   (apply = (sum-by-cols vs))
+   (apply = (sum-by-diagonals vs))))
+
+(defn values->square [vs]
+  (->> vs
+       (partition 3)
+       (map vec)
+       (into [])))
+
 (defn magic-square [values]
-  [[1.0 1.5 2.0]
-   [2.5 3.0 3.5]
-   [4.0 4.5 5.0]])
+  (->> values
+       combo/permutations
+       (filter magic?)
+       first
+       values->square))
