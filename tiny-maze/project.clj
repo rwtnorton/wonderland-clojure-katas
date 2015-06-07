d96b28dada08fd948a164b3c14c996e265b46505
Richard W. Norton <rwtnorton@gmail.com>
2015-06-07 00:10:40 -0700

    Update profile description, etc.

--- tiny-maze/project.clj
+++ tiny-maze/project.clj
@@ -1,6 +1,6 @@
 (defproject tiny-maze "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "My solution to the Tiny Maze Kata from Learning Clojure."
+  :url "https://github.com/rwtnorton/wonderland-clojure-katas/tree/master/tiny-maze"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
